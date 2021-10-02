6d7b7bc972a415f6a71bdf576fe75f901a8fe159
freelancernoor <info@devnurul.me>
2021-10-03 01:11:25 +0600

    react project commit

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Shop = () => {
+    const [ course, setCourse] = useState([]);
+
+    useEffect(() => { 
+        fetch("../Course/courseData.json")
+        .then( res => res.json())
+        .then(data =>{
+            console.log(data);;
+        })
+    }, [])
     return (
         <div>
-            
+            <h2> Welcone to LFWF</h2>
         </div>
     );
 };
